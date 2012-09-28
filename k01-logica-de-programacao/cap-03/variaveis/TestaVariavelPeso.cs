d2838aceb0dc2be15aae93e5809f7a47a2b36eea
ftamashiro <tamashiro_jundy4@hotmail.com>
2012-09-28 11:57:51 -0300

    Adicionando exercicios complementares ao K01 - Variaveis.

---
+++ k01-logica-de-programacao/cap-03/variaveis/TestaVariavelPeso.cs
@@ -0,0 +1,11 @@
+class TestaVariavelPeso 
+{
+	static void Main() 
+	{
+		double peso;
+		
+		peso = 88.20;
+		
+		System.Console.WriteLine(peso);
+	}
+}
---
+++ k01-logica-de-programacao/cap-03/variaveis/TestaVariavelPeso.java
@@ -0,0 +1,9 @@
+class TestaVariavelPeso {
+	public static void main(String[] args) {
+		double peso;
+		
+		peso = 88.20;
+		
+		System.out.println(peso);
+	}
+}
